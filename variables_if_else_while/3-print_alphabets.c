05a94697b960c5c6d68e36035115deaa5e76cd1b
youssefzaguia <youssefultras02tn@gmail.com>
2023-10-30 09:14:36 +0100

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main-prints the alphabet in lowercase and then in uppercase
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+}
