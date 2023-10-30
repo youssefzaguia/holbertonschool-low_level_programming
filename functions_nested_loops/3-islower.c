8f6bfe5b4f8fe2058b0c67dd2a69a0abc10ad2ae
youssefzaguia <youssefultras02tn@gmail.com>
2023-10-30 10:03:19 +0100

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ * @c : integer
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
