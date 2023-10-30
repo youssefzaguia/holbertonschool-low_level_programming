40f3e1f1466831945af3a28bbf680ef5afad2328
youssefzaguia <youssefultras02tn@gmail.com>
2023-10-30 10:04:49 +0100

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha -  prints if alpha, followed by a new line
+ * Return: Always 0 (Success)
+ * @c : integer
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 65 && c <=122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
