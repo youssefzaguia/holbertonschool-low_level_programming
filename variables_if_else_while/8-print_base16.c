22e66ffce65a186c3449aeab7f0a470c785eb95c
youssefzaguia <youssefultras02tn@gmail.com>
2023-10-30 09:25:30 +0100

    program that prints all the numbers of base

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main-prints all the numbers of base 16 in lowercase
+ *Return: 1
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
