28aee47e28a070592ead558c066c4fac2c523487
youssefzaguia <youssefultras02tn@gmail.com>
2023-10-30 09:26:50 +0100

    program that prints all possible

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main-prints all possible combinations of single-digit numbers
+ *Return: 1
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
