46b852ee58c13f245c579b316cfc519e04fb53fb
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-03 18:44:48 +0100

    Great leaders

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _atoi - Convert string to an integer.
+ * @s: Pointer to a character string.
+ *
+ * Return: void.
+ */
+
+int _atoi(char *s)
+{
+	int sign;
+	unsigned int num;
+	char *temp;
+
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do
+		{
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
+}
