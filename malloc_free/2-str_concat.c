5ad7cea96e03dc5792433d97e2b67377bfca2b2b
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-07 12:08:27 +0100

    He who is not courageous

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,26 +9,24 @@
  *
  * Return: pointer to new string
  */
-strcat(char *dest, const char *src)
+char *str_concat(char *s1, char *s2)
 {
-    int size = 0;
-	char *ptr, *ret;
+	char *pstr;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-	if (!str)
-		return (NULL);
-
-	ptr = str;
-	while (*ptr++)
-		size++;
-
-	ret = malloc(size + 1);
-	if (!ret)
-		return (NULL);
-
-	ptr = ret;
-	while (*str)
-		*ptr++ = *str++;
-
-	*ptr = 0;
-	return (ret);
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	while (s1[len1] != 0)
+		len1++;
+	while (s2[len2] != 0)
+		len2++;
+	len2++;
+	pstr = malloc(sizeof(char) * (len1 + len2));
+	for (i = 0; i < len1; i++)
+		pstr[i] = s1[i];
+	for (j = 0; j < len2; i++, j++)
+		pstr[i] = s2[j];
+	return (pstr);
 }
