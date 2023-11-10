8d175133770a8dc9c697a14b5d36335a104df159
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-10 10:27:09 +0100

    mainh

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,8 +1,14 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+
+#endif
