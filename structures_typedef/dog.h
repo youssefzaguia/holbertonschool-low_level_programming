2aef2caeafad9e584e88161d0b8706ab8a9fb89d
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-13 13:30:29 +0100

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -type struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
