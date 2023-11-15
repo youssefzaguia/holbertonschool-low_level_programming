37077c142c0c4f2c4847781468d0febc954b081b
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-15 09:44:39 +0100

    dog is love

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,1 +1,21 @@
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -type struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
