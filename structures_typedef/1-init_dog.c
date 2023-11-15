aace086d6805d432465e5f8b7b6aae960b0e88fc
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-15 09:57:40 +0100

    dog is the only thing

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,23 +1,17 @@
-#ifndef DOG_H
-#define DOG_H
-#include <stdio.h>
 #include "dog.h"
 /**
- * struct dog -type struct
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of the dog
+ * init_dog - function that initialize a variable
+ * @d: ptr
+ * @name: dog namee
+ * @age: dog age
+ * @owner: dog owner
  */
-
-typedef struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
