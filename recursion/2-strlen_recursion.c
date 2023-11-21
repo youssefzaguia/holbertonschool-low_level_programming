0a7c6d6f294d48e3e84f1c775bad2ce6160908d8
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-21 13:35:16 +0100

    Dreams feel real

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s : pointeur
+ *Return: n
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+	{
+		n += _strlen_recursion(s + 1) + 1;
+	}
+	return (n);
+}
