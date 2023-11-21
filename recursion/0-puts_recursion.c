6b0dc142cf4f64d9d48d4d588af1082990dc5071
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-21 13:29:13 +0100

    She locked away a secret

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * _puts_ - function that prints a string, followed a new line
+ *  @s :pointeur
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
