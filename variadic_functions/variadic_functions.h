89eec64c6527a80d538968d6df4dba5737c14c5b
youssefzaguia <youssefultras02tn@gmail.com>
2023-11-21 13:23:26 +0100

    x
    :x
    :wq

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,5 +9,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
