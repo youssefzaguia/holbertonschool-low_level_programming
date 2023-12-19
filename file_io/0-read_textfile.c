95743fd494290948343aa254f07c12a6ebd35a1d
youssefzaguia <youssefultras02tn@gmail.com>
2023-12-19 10:33:56 +0100

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,41 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
-
+#include "main.h"
 /**
- * read_textfile - that reads a text file and prints
- * @filename: variable pointer
- * @letters: size letters
- * Description: Write a function that reads a text file and prints it
- * to the POSIX standard output.
- * Return: the actual number of letters it could read and print, 0 otherwise
+ * read_textfile - a function that reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename : the name of the file
+ * @letters: letters to print
+ * Return: the actual number of letters it could read and print (bw)
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
+	int file;
+	int br, bw;
+
 	char *text;
 
-	text = malloc(letters);
-	if (text == NULL)
+	if (filename == NULL)
 		return (0);
 
-	if (filename == NULL)
+	text = malloc(sizeof(char) * letters);
+	if (text == NULL)
 		return (0);
 
 	file = open(filename, O_RDONLY);
 
 	if (file == -1)
+		return (0);
+
+	br = read(file, text, letters);
+	if (br == -1)
 	{
+		close(file);
 		free(text);
 		return (0);
 	}
+	bw = write(STDOUT_FILENO, text, br);
 
-	let = read(file, text, letters);
-
-	w = write(STDOUT_FILENO, text, let);
+	if (bw == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
 
 	close(file);
-
-	return (w);
+	return (bw);
 }
